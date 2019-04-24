586103b9d8d7100d6fbc7d315fa165dbac9edef6
Madave94 <david.tschirschwitz@uni-weimar.de>
2019-04-24 19:26:11 +0200

    included tests for rot13

--- src/test/java/common/TestEncryption.java
+++ src/test/java/common/TestEncryption.java
@@ -1,11 +1,24 @@
 package common;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class TestEncryption {
 	
 	@Test
 	public void testROT13encryption() {
+		String encryptMe = "Hallo wie gehts!";
+		String result = new ROT13().encrypt(encryptMe);
+		String expected = "Unyyb jvr trugf!";
+		assertEquals(true, expected.equals(result));
+	}
+	
+	@Test
+	public void testROT13decryption() {
+		String decryptMe = "Zve trugf thg!";
+		String result = new ROT13().decrypt(decryptMe);
+		String expected = "Mir gehts gut!";
+		assertEquals(true, expected.equals(result));
 		
 	}
 
