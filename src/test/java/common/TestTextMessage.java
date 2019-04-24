95affc72e84711861d017196aeddeedd0efc796b
Madave94 <david.tschirschwitz@uni-weimar.de>
2019-04-24 19:42:34 +0200

    Tests for textmessage.equals added

---
+++ src/test/java/common/TestTextMessage.java
@@ -0,0 +1,21 @@
+package common;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class TestTextMessage {
+	
+	@Test
+	public void testEqualsTrue() {
+		TextMessage msg1 = new TextMessage("Das ist ein Test.");
+		TextMessage msg2 = new TextMessage("Das ist ein Test.");
+		assertEquals(true, msg1.equals(msg2));		
+	}
+	
+	@Test
+	public void testEqualsFalse() {
+		TextMessage msg1 = new TextMessage("Das ist ein Test.");
+		TextMessage msg2 = new TextMessage("Das ist nicht gleich.");
+		assertEquals(false, msg1.equals(msg2));		
+	}
+}
