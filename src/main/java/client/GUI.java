6acd64394c5cab5feb7e1fdbf4d79f437e1993cf
Madave94 <david.tschirschwitz@uni-weimar.de>
2019-05-10 15:45:39 +0200

    adding hello-world GUI and adjusting build.gradle file

--- src/main/java/client/GUI.java
+++ src/main/java/client/GUI.java
@@ -1,8 +1,15 @@
 package client;
 
-public class GUI implements ChatLineListener, Runnable{
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class GUI extends Application implements ChatLineListener, Runnable{
 	
 	// stub for GUI
+	public GUI () {}
 	
 	public GUI (Client client) {
 		// TODO
@@ -18,5 +25,19 @@ public class GUI implements ChatLineListener, Runnable{
 	public void newChatLine(String line) {
 		// TODO Auto-generated method stub
 	}	
+	
+   @Override
+    public void start(Stage stage) {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Scene scene = new Scene(new StackPane(l), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 
 }
